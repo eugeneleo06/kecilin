e1f40988091ee53cc0eb44ee043d26f83199e3ef
eugeneleo06 <eugeneleo14@gmail.com>
2024-02-27 12:19:40 +0700

    add size in automation

--- automation/run.py
+++ automation/run.py
@@ -6,6 +6,7 @@ import pandas as pd
 import cv2
 from skimage.metrics import structural_similarity as ssim
 from skimage.metrics import peak_signal_noise_ratio as psnr
+import os
 
 quality = [90,80,70,60,50,40]
 
@@ -48,12 +49,14 @@ def get_paths(format, quality=None):
 def compare_psnr_ssim(bmp_files, files):
     psnr_arr = []
     ssim_arr = []
+    size_arr = []
 
     n = len(bmp_files)
 
     for x in range (n):
         img_ori = cv2.imread(bmp_files[x])
         img_comp = cv2.imread(files[x])
+        
 
         img_ori = cv2.cvtColor(img_ori, cv2.COLOR_BGR2GRAY)
         img_comp = cv2.cvtColor(img_comp, cv2.COLOR_BGR2GRAY)
@@ -63,10 +66,18 @@ def compare_psnr_ssim(bmp_files, files):
 
         psnr_value = psnr(img_ori_arr, img_comp_arr, data_range=255)
         ssim_value, _ = ssim(img_ori_arr, img_comp_arr, full=True, data_range=255)
+
+        # Get the file size in bytes
+        file_size_bytes = os.path.getsize(files[x])
+
+        # Convert to kilobytes (1 KB = 1024 bytes)
+        file_size_kb = file_size_bytes / 1024
         
         psnr_arr.append(f"{psnr_value:.4f}")
         ssim_arr.append(f"{ssim_value:.5f}")
-    return psnr_arr, ssim_arr
+        size_arr.append(f"{file_size_kb:.4f}")
+
+    return psnr_arr, ssim_arr, size_arr
         
 bmp_files = get_paths('bmp')
 jpeg_files = get_paths('jpeg')
@@ -78,13 +89,13 @@ webp_80_files = get_paths('webp_80', 80)
 webp_90_files = get_paths('webp_90', 90)
 
 
-psnr_jpeg, ssim_jpeg = compare_psnr_ssim(bmp_files, jpeg_files)
-psnr_40, ssim_40 = compare_psnr_ssim(bmp_files, webp_40_files)
-psnr_50, ssim_50 = compare_psnr_ssim(bmp_files, webp_50_files)
-psnr_60, ssim_60 = compare_psnr_ssim(bmp_files, webp_60_files)
-psnr_70, ssim_70 = compare_psnr_ssim(bmp_files, webp_70_files)
-psnr_80, ssim_80 = compare_psnr_ssim(bmp_files, webp_80_files)
-psnr_90, ssim_90 = compare_psnr_ssim(bmp_files, webp_90_files)
+psnr_jpeg, ssim_jpeg, size_jpeg = compare_psnr_ssim(bmp_files, jpeg_files)
+psnr_40, ssim_40, size_40 = compare_psnr_ssim(bmp_files, webp_40_files)
+psnr_50, ssim_50, size_50 = compare_psnr_ssim(bmp_files, webp_50_files)
+psnr_60, ssim_60, size_60 = compare_psnr_ssim(bmp_files, webp_60_files)
+psnr_70, ssim_70, size_70 = compare_psnr_ssim(bmp_files, webp_70_files)
+psnr_80, ssim_80, size_80 = compare_psnr_ssim(bmp_files, webp_80_files)
+psnr_90, ssim_90, size_90 = compare_psnr_ssim(bmp_files, webp_90_files)
 
 
 seq = []
@@ -95,36 +106,43 @@ data_jpeg = {
     'Sequence': seq,
     'PSNR': psnr_jpeg,
     'SSIM': ssim_jpeg,
+    'Size (KB)': size_jpeg,
 }
 data_40 = {
     'Sequence': seq,
     'PSNR': psnr_40,
     'SSIM': ssim_40,
+    'Size (KB)': size_40,
 }
 data_50 = {
     'Sequence': seq,
     'PSNR': psnr_50,
     'SSIM': ssim_50,
+    'Size (KB)': size_50,
 }
 data_60 = {
     'Sequence': seq,
     'PSNR': psnr_60,
     'SSIM': ssim_60,
+    'Size (KB)': size_60,
 }
 data_70 = {
     'Sequence': seq,
     'PSNR': psnr_70,
     'SSIM': ssim_70,
+    'Size (KB)': size_70,
 }
 data_80 = {
     'Sequence': seq,
     'PSNR': psnr_80,
     'SSIM': ssim_80,
+    'Size (KB)': size_80,
 }
 data_90 = {
     'Sequence': seq,
     'PSNR': psnr_90,
     'SSIM': ssim_90,
+    'Size (KB)': size_90,
 }
 
 # Creating DataFrames
